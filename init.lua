be65133e877cc6f175e6712e8b4921173decce96
blenderboi <blenderboi2018@gmail.com>
2024-05-05 19:36:45 +0800

    a bunch of stuff

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ local opt = vim.opt
 opt.backup = false
 opt.clipboard:append("unnamedplus")
 opt.cmdheight = 2
-opt.completeopt = {"menuone", "noselect"}
+opt.completeopt = { "menuone", "noselect" }
 opt.conceallevel = 0
 opt.fileencoding = "utf-8"
 opt.hlsearch = true
@@ -88,6 +88,15 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
+
+
+{"0x00-ketsu/maximizer.nvim",
+  config = function()
+    require("maximizer").setup()
+ 
+  end},
+
+
   {
   "folke/tokyonight.nvim", 
   priotiy = 1000,
@@ -100,21 +109,92 @@ require("lazy").setup({
 
     end
   },
+{
+  'stevearc/dressing.nvim',
+  opts = {},
+},
 
+{"nvim-treesitter/nvim-treesitter", build=":TSUpdate", dependencies={"windwp/nvim-ts-autotag"}, 
+config =  function()
+  local treesitter = require("nvim-treesitter.configs")
+  treesitter.setup({
+    highlight = {enable = true},
+    indent = {enable = true},
+    autotag = {enable = true},
+    ensure_installed = {"lua", "python"},
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-]>",
+        node_incremental= "<C-]>",
+        scope_incremental = false,
+        node_decremental = "<bs>"
+
+      }
+
+    }
+
+  })
+end
+},
+
+{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
+config = function()
+  require("bufferline").setup(
   {
-  "lewis6991/gitsigns.nvim", 
-  config = function()
-    require("gitsigns").setup()
-    end
-  },
+        options = {
+
+        offsets={{
+
+          filetype="NvimTree",
+          text="Nvim Tree",
+          highlight = "Directory"
+        }
+
+        }
+        }
+
+      }
+  )
+end
+
+},
 
 
+    {"stevearc/conform.nvim", 
+    config=function() 
+      local conform = require("conform")
+      conform.setup({
+        formatters_by_ft = {
+          javascript = {"prettier"},
+          typescript = {"prettier"},
+          svelte = {"prettier"},
+          css = {"prettier"},
+          html = {"prettier"},
+          json = {"prettier"},
+          yaml = {"prettier"},
+          markdown = {"prettier"},
+          lua = {"stylua"},
+          python = {"isort", "black"}
 
+        },
+
+        vim.keymap.set({"n", "v"}, "<A-w>",
+        function()
+            conform.format()
+
+        end)
+
+      })
+    end},
 
   {"nvim-lualine/lualine.nvim", 
     dependencies = {"nvim-tree/nvim-web-devicons"},
     config = function()
-      require("lualine").setup()
+      require("lualine").setup({
+
+
+      })
     end
 
   },
@@ -151,20 +231,119 @@ require("lazy").setup({
 -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' },
       config = function()
-        require("telescope").setup{
-          pickers = {
-            find_files = {theme = "dropdown"},
-            registers = {theme = "cursor"},
-            buffers = {theme = "ivy"}
-          }
-
-        }
+	require("telescope").setup({
+		pickers = {
+			find_files = { theme = "dropdown" },
+			registers = { theme = "cursor" },
+			buffers = { theme = "ivy" },
+		},
+	})
       end
+
+    
 
     },
 
+    {
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+      dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      {"L3MON4D3/LuaSnip", version="v2.*", build = "make install_jsregexp"},
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
+      "chrisgrieser/cmp-nerdfont",
+      "hrsh7th/cmp-calc",
+      "amarakon/nvim-cmp-fonts",
+      "roginfarrer/cmp-css-variables"
 
 
+
+      },
+      config = function()
+	local cmp = require("cmp")
+	local luasnip = require("luasnip")
+	local lspkind = require("lspkind")
+	require("luasnip.loaders.from_vscode").lazy_load()
+        cmp.setup({
+          completion = {
+            completeopt = "menu,menuone,preview,noselect",
+          },
+          snippets = {
+            expand = function(args)
+		luasnip.lsp_expand(args.body)
+            end,
+
+          },
+      sources = cmp.config.sources({
+        {name="nvim_lsp"},
+        {name="luasnip"},
+        {name="buffer"},
+        {name="path"},
+        {name="calc"},
+        {name="css-variables"},
+        {name= "fonts", option = { space_filter = "-" } },
+        {name="nerdfont"}
+      }),
+      formatting = {
+        format = lspkind.cmp_format({
+          maxwidth=50, 
+          ellipsis_char = "...",
+
+        })
+
+      },
+
+ mapping = {
+
+    -- ... Your other mappings ...
+   ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                cmp.confirm({
+                    select = true,
+                })
+            end
+        else
+            fallback()
+        end
+    end),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item()
+		elseif luasnip.locally_jumpable(-1) then
+			luasnip.jump(-1)
+		else
+			fallback()
+		end
+    end, { "i", "s" }),
+
+    -- ... Your other mappings ...
+  },
+
+        
+  })
+        
+    
+      end
+
+
+    },
 
 
 {
@@ -174,14 +353,59 @@ require("lazy").setup({
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
 },
-{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+    
+
+-- {
+--     'windwp/nvim-autopairs',
+--     event = "InsertEnter",
+--     dependencies = {"hrsh7th/nvim-cmp"},
+--     config = function()
+--       local autopairs = require("nvim-autopairs")
+--       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+--       local cmp = require("cmp")
+--       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+--     end
+--     -- use opts = {} for passing setup options
+--     -- this is equalent to setup({}) function
+-- },
+--
+{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+
+
+
+
+
+{"williamboman/mason.nvim", dependencies={"williamboman/mason-lspconfig.nvim"},
+config=function()
+	local mason = require("mason")
+	mason.setup()
+end
+},
+
+
+{"neovim/nvim-lspconfig", dependencies={"hrsh7th/cmp-nvim-lsp", {"antosha417/nvim-lsp-file-operations", config=true}, {"folke/neodev.nvim", opts={}}}
+, config = function() 
+	local lspconfig = require("lspconfig")
+	local mason_lspconfig = require("mason-lspconfig")
+	mason_lspconfig.setup()
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	local capabilities = cmp_nvim_lsp.default_capabilities()
+	mason_lspconfig.setup_handlers({
+		function(server_name)
+			lspconfig[server_name].setup({ capabilities = capabilities })
+		end,
+	})
+end},
+
+
 
 })
 
-
 keymap.set("n", "<A-t>", "<cmd>NvimTreeToggle<CR>")
-local builtin = require('telescope.builtin')
-keymap.set('n', '<A-h>', builtin.oldfiles, {})
-keymap.set('n', '<A-b>', builtin.buffers, {})
-keymap.set('n', '<A-p>', builtin.registers, {})
-keymap.set('n', '<A-f>', builtin.find_files, {})
+local builtin = require("telescope.builtin")
+keymap.set("n", "<A-h>", builtin.oldfiles, {})
+keymap.set("n", "<A-b>", builtin.buffers, {})
+keymap.set("n", "<A-p>", builtin.registers, {})
+keymap.set("n", "<A-f>", builtin.find_files, {})
+
+vim.api.nvim_set_keymap('n', '<A-m>', '<cmd>lua require("maximizer").toggle()<CR>', {silent = true, noremap = true})
