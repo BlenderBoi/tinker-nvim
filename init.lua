3363c89a6068e8c454036eba51e1478ee53166dc
blenderboi <blenderboi2018@gmail.com>
2024-05-08 22:28:42 +0800

    updated formatting for completion

--- init.lua
+++ init.lua
@@ -42,9 +42,38 @@ opt.background = "dark"
 -- opt.backspace = "indent,eol,start"
 
 
+local kind_icons = {
+  Text = "󰉿",
+	Method = "󰆧",
+	Function = "󰊕",
+	Constructor = "",
+  Field = " ",
+	Variable = "󰀫",
+	Class = "󰠱",
+	Interface = "",
+	Module = "",
+	Property = "󰜢",
+	Unit = "󰑭",
+	Value = "󰎠",
+	Enum = "",
+	Keyword = "󰌋",
+  Snippet = "",
+	Color = "󰏘",
+	File = "󰈙",
+  Reference = "",
+	Folder = "󰉋",
+	EnumMember = "",
+	Constant = "󰏿",
+  Struct = "",
+	Event = "",
+	Operator = "󰆕",
+  TypeParameter = " ",
+	Misc = " ",
+}
 
 
 local keymap = vim.keymap
+
 
 local opts = {noremap = true, silent = true}
 
@@ -289,14 +318,38 @@ end
         {name= "fonts", option = { space_filter = "-" } },
         {name="nerdfont"}
       }),
+      -- formatting = {
+      --   format = lspkind.cmp_format({
+      --      -- mode = "symbol",
+      --      maxwidth=50, 
+      --      ellipsis_char = "...",
+      --      show_labelDetails = true,
+      --    })},
+      --
+      
+
       formatting = {
-        format = lspkind.cmp_format({
-          maxwidth=50, 
-          ellipsis_char = "...",
-
-        })
-
+        fields = { "abbr", "kind", "menu" },
+        format = function(entry, vim_item)
+          -- Kind icons
+          -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+          vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+          vim_item.menu = ({
+            nvim_lsp = "[LSP]",
+            luasnip = "[Snippet]",
+            buffer = "[Buffer]",
+            path = "[Path]",
+          })[entry.source.name]
+          return vim_item
+        end,
       },
+
+
+
+
+
+
+
 
  mapping = {
 
