38fea66f14e9144d8e64c3745976c60aef1e4c56
blenderboi <blenderboi2018@gmail.com>
2024-05-11 10:12:44 +0800

    rename conform to formatting

---
+++ lua/plugin-config/formatting.lua
@@ -0,0 +1,29 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+
+			vim.keymap.set({ "n", "v" }, "<A-w>", function()
+				conform.format()
+			end),
+		})
+	end,
+}
