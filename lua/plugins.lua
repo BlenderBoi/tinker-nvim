6b9969e954dad5c244d3a6bb7a3ec8c3ce6a7f99
blenderboi <blenderboi2018@gmail.com>
2024-05-11 10:13:24 +0800

    update plugin.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,21 +1,20 @@
-
-require "plugin-config.plugin-manager"
+require("plugin-config.plugin-manager")
 
 require("lazy").setup({
 
-  require "plugin-config.maximizer",
-  require "plugin-config.colorscheme",
-  require "plugin-config.dressing",
-  require "plugin-config.treesitter",
-  require "plugin-config.bufferline",
-  require "plugin-config.conform",
-  require "plugin-config.lualine",
-  require "plugin-config.comment",
-  require "plugin-config.nvim-tree",
-  require "plugin-config.autopairs",
-  require "plugin-config.telescope",
-  require "plugin-config.indent-blankline",
-  require "plugin-config.mason",
-  require "plugin-config.lsp",
-  require "plugin-config.completion",
+	require("plugin-config.maximizer"),
+	require("plugin-config.colorscheme"),
+	require("plugin-config.dressing"),
+	require("plugin-config.treesitter"),
+	require("plugin-config.bufferline"),
+	require("plugin-config.formatting"),
+	require("plugin-config.lualine"),
+	require("plugin-config.comment"),
+	require("plugin-config.nvim-tree"),
+	require("plugin-config.autopairs"),
+	require("plugin-config.telescope"),
+	require("plugin-config.indent-blankline"),
+	require("plugin-config.mason"),
+	require("plugin-config.lsp"),
+	require("plugin-config.completion"),
 })
